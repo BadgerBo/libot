162a04040b6f3b29189a253e91c65f88a6675b46
Olesya <0099bo@gmail.com>
2020-02-12 14:45:39 +0300

    Added new version (goodreads.com)

--- libot/bot.py
+++ libot/bot.py
@@ -10,76 +10,134 @@ bot = telebot.TeleBot(token)
 @bot.message_handler(commands=['start'])
 def provide_command_start(message):
     keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
-    g = telebot.types.KeyboardButton(text='Жанры')
-    t = telebot.types.KeyboardButton(text='Топ-100')
-    r = telebot.types.KeyboardButton(text='Случайный выбор')
-    keyboard.row(g)
-    keyboard.row(t, r)
-    bot.send_message(
-        message.chat.id,
-        f'Привет, {message.chat.username} \nЯ помогу тебе с выбором книги\n' +
-        'Чтобы начать, выбери одну из доступных опций \n\n1)   Жанры\n2)   Топ-100\n3)   Случайный выбор',
-        parse_mode="Markdown", reply_markup=keyboard)
+    genres_button = telebot.types.KeyboardButton(text='Genres')
+    random_button = telebot.types.KeyboardButton(text='Random')
+    choice_awards_button = telebot.types.KeyboardButton(text='Choice Awards')
+    new_releases_button = telebot.types.KeyboardButton(text='New Releases')
+    keyboard.row(genres_button, random_button)
+    keyboard.row(choice_awards_button, new_releases_button)
+    bot.send_message(message.chat.id,
+                     'Greetings, {}\n'.format(message.chat.username) +
+                     'I can help you choose your next book\nWhat do you want to read?',
+                     parse_mode="Markdown", reply_markup=keyboard)
 
 
 @bot.message_handler(commands=['help'])
 def provide_command_help(message):
     keyboard = telebot.types.InlineKeyboardMarkup()
-    keyboard.add(telebot.types.InlineKeyboardButton('Связаться с хозяином бота', url='https://t.me/BadgerBo'))
-    bot.send_message(
-        message.chat.id,
-        'Выбери одну из доступных опций \n\n1)   Жанры\n2)   Топ-100\n3)   Случайный выбор' +
-        '\n\nЕсли твой выбор пал на жанры, то определись с тем, какой из них интересует больше всего',
-        parse_mode="Markdown", reply_markup=keyboard)
+    keyboard.add(telebot.types.InlineKeyboardButton('Contact bot\'s creator', url='https://t.me/BadgerBo'))
+    bot.send_message(message.chat.id,
+                     'Choose one of the available options:\n1)  Genres\n2)  Random\n3)  Choice Awards\n' +
+                     '4)  New Releases\nThen choose particular genre or year of publication, if necessary',
+                     parse_mode="Markdown", reply_markup=keyboard)
 
 
 @bot.message_handler(content_types=["text"])
 def handle_general_choice(message):
-    if message.text == 'Жанры':
-        provide_genres_choice(message)
-    elif message.text == 'Топ-100':
-        top_book = parser.compile_advice_top()
-        give_book_advice(message, top_book, None)
-    elif message.text == 'Случайный выбор':
-        random_book = parser.compile_advice_random()
-        give_book_advice(message, random_book, None)
+    if message.text == 'Genres':
+        provide_genre_choice(message)
+    elif message.text == 'Random':
+        try:
+            random_book = parser.compile_advice_random()
+            give_book_advice(message, random_book, None)
+        except:
+            random_book = parser.compile_advice_random()
+            give_book_advice(message, random_book, None)
+    elif message.text == 'Choice Awards':
+        provide_year_choice(message)
+    elif message.text == 'New Releases':
+        try:
+            new_releases_book = parser.compile_advice_new_releases()
+            give_book_advice(message, new_releases_book, None)
+        except:
+            new_releases_book = parser.compile_advice_new_releases()
+            give_book_advice(message, new_releases_book, None)
     else:
         pass
 
 
-def provide_genres_choice(message):
+def provide_genre_choice(message):
     keyboard = telebot.types.InlineKeyboardMarkup()
     keyboard.row(
-        telebot.types.InlineKeyboardButton(text='Классика', callback_data='Классическая-литература'),
-        telebot.types.InlineKeyboardButton(text='Детективы', callback_data='Детективы'),
-        telebot.types.InlineKeyboardButton(text='Для детей', callback_data='Детские-книги')
+        telebot.types.InlineKeyboardButton(text='Biography', callback_data='biography'),
+        telebot.types.InlineKeyboardButton(text='Children\'s', callback_data='children-s'),
+        telebot.types.InlineKeyboardButton(text='Classics', callback_data='classics'),
     )
     keyboard.row(
-        telebot.types.InlineKeyboardButton(text='Фантастика', callback_data='Фантастика'),
-        telebot.types.InlineKeyboardButton(text='Приключения', callback_data='Приключения'),
-        telebot.types.InlineKeyboardButton(text='Ужасы', callback_data='Ужасы-мистика')
+        telebot.types.InlineKeyboardButton(text='Fantasy', callback_data='fantasy'),
+        telebot.types.InlineKeyboardButton(text='Fiction', callback_data='fiction'),
+        telebot.types.InlineKeyboardButton(text='Graphic Novels', callback_data='graphic-novels'),
     )
     keyboard.row(
-        telebot.types.InlineKeyboardButton(text='Любовные романы', callback_data='Любовные-романы'),
-        telebot.types.InlineKeyboardButton(text='Фэнтези', callback_data='Фэнтези'),
-        telebot.types.InlineKeyboardButton(text='Поэзия', callback_data='Поэзия-и-драматургия')
+        telebot.types.InlineKeyboardButton(text='Horror', callback_data='horror'),
+        telebot.types.InlineKeyboardButton(text='Mystery', callback_data='mystery'),
+        telebot.types.InlineKeyboardButton(text='Nonfiction', callback_data='non-fiction'),
     )
-    bot.send_message(message.chat.id, 'Выберите интересующий жанр', reply_markup=keyboard)
+    keyboard.row(
+        telebot.types.InlineKeyboardButton(text='Poetry', callback_data='poetry'),
+        telebot.types.InlineKeyboardButton(text='Romance', callback_data='romance'),
+        telebot.types.InlineKeyboardButton(text='Science Fiction', callback_data='science-fiction'),
+    )
+    keyboard.row(
+        telebot.types.InlineKeyboardButton(text='Thriller', callback_data='thriller'),
+        telebot.types.InlineKeyboardButton(text='Travel', callback_data='travel'),
+        telebot.types.InlineKeyboardButton(text='Young Adult', callback_data='young-adult'),
+    )
+    bot.send_message(message.chat.id, 'In which genre do you take an interest?', reply_markup=keyboard)
+
+
+def provide_year_choice(message):
+    keyboard = telebot.types.InlineKeyboardMarkup()
+    keyboard.row(
+        telebot.types.InlineKeyboardButton(text='2019', callback_data='2019'),
+        telebot.types.InlineKeyboardButton(text='2018', callback_data='2018'),
+        telebot.types.InlineKeyboardButton(text='2017', callback_data='2017'),
+    )
+    keyboard.row(
+        telebot.types.InlineKeyboardButton(text='2016', callback_data='2016'),
+        telebot.types.InlineKeyboardButton(text='2015', callback_data='2015'),
+        telebot.types.InlineKeyboardButton(text='2014', callback_data='2014'),
+    )
+    keyboard.row(
+        telebot.types.InlineKeyboardButton(text='2013', callback_data='2013'),
+        telebot.types.InlineKeyboardButton(text='2012', callback_data='2012'),
+        telebot.types.InlineKeyboardButton(text='2011', callback_data='2011'),
+    )
+    bot.send_message(message.chat.id, 'Winners of Annual Goodreads Choice Awards', reply_markup=keyboard)
 
 
 @bot.callback_query_handler(func=lambda call: True)
-def handle_choice_of_genre(call):
-    genre = call.data
-    genre_book = parser.compile_advice_genre(genre)
-    give_book_advice(call.message, genre_book, genre)
+def handle_choice_of_genre_or_year(call):
+    if call.data[0:1] == '20':
+        year = call.data
+        try:
+            choice_awards_book = parser.compile_advice_choice_awards(year)
+            give_book_advice(call.message, choice_awards_book, year)
+        except:
+            choice_awards_book = parser.compile_advice_choice_awards(year)
+            give_book_advice(call.message, choice_awards_book, year)
+    else:
+        genre = call.data
+        try:
+            genre_book = parser.compile_advice_genre(genre)
+            give_book_advice(call.message, genre_book, genre)
+        except:
+            genre_book = parser.compile_advice_genre(genre)
+            give_book_advice(call.message, genre_book, genre)
 
 
-def give_book_advice(message, choice, genre):
+def give_book_advice(message, book, flag_for_additional_option):
     keyboard = telebot.types.InlineKeyboardMarkup()
-    keyboard.add(telebot.types.InlineKeyboardButton('Подробнее о книге', url=choice[0]))
-    if genre != None:
-        keyboard.add(telebot.types.InlineKeyboardButton('Еще книги этого жанра', callback_data=genre))
-    bot.send_photo(message.chat.id, choice[1], choice[2], parse_mode="Markdown", reply_markup=keyboard)
+    keyboard.add(telebot.types.InlineKeyboardButton('View on goodreads.com', url=book[0]))
+    if flag_for_additional_option is None:
+        pass
+    elif flag_for_additional_option[0].isalpha():
+        keyboard.add(telebot.types.InlineKeyboardButton('Another book of this genre',
+                                                        callback_data=flag_for_additional_option))
+    elif flag_for_additional_option[0].isdigit():
+        keyboard.add(telebot.types.InlineKeyboardButton('Another best book of {}'.format(flag_for_additional_option),
+                                                        callback_data=flag_for_additional_option))
+    bot.send_photo(message.chat.id, book[1], book[2], parse_mode="Markdown", reply_markup=keyboard)
 
 
 if __name__ == '__main__':
--- libot/parser.py
+++ libot/parser.py
@@ -3,96 +3,73 @@ import requests
 import random
 
 
+class Book:
+
+    def __init__(self, soup):
+        self.soup = soup
+
+    def get_url(self):
+        return self.soup.find('link', {'rel': "canonical"}).get('href')
+
+    def get_cover(self):
+        return self.soup.find('img', {'id': "coverImage"}).get('src')
+
+    def get_description(self):
+        title = self.soup.find('h1', {'itemprop': "name"}).get_text().strip('\n ')
+        author = self.soup.find('span', {'itemprop': "name"}).get_text()
+        rating = self.soup.find('span', {'itemprop': "ratingValue"}).get_text().strip('\n ')
+        synopsis = self.soup.find('span', {'style': "display:none"}).get_text()[:150] + '...'
+        return '*{}*\nby {}\n\nRating: {}\n\n{}'.format(title, author, rating, synopsis)
+
+
+def get_full_book_info(book_soup):
+    book = Book(book_soup)
+    url = book.get_url()
+    cover = book.get_cover()
+    description = book.get_description()
+    return url, cover, description
+
+
 def get_soup(url):
-	raw_html = requests.get(url).content
-	return BeautifulSoup(raw_html, "html.parser")
+    raw_html = requests.get(url).content
+    return BeautifulSoup(raw_html, "html.parser")
 
 
-def compile_advice_genre(data):
-	'''Жанры'''
-	try:
-		url_genre = get_page_soup(data)
-		soup_of_genre_page = get_soup(url_genre)
-		raw_book_html = make_choice_from_page(soup_of_genre_page)
-		soup_of_book = get_soup('https://www.livelib.ru/' + raw_book_html)
-		description = f'{get_book_title(soup_of_book)}\n\n{get_book_author(soup_of_book)}\n\n{get_book_info(soup_of_book)}\n\n{get_book_rating(soup_of_book)}'
-		link = get_book_link(soup_of_book)
-		cover = get_book_cover(soup_of_book)
-		return link, cover, description
-	except:
-		compile_advice_genre(data)
+def make_book_choice_from_page(page_soup):
+    list_of_books = []
+    for book in page_soup.find_all('a', {'class': "bookTitle"}):
+        book = book.get('href')
+        list_of_books.append(book)
+    return random.choice(list_of_books)
 
 
-def compile_advice_top():
-	'''Топ-100'''
-	try:
-		soup_of_top = get_soup('https://www.livelib.ru/books/top')
-		raw_book_html = make_choice_from_page(soup_of_top)
-		soup_of_book = get_soup('https://www.livelib.ru/' + raw_book_html)
-		description = f'{get_book_title(soup_of_book)}\n\n{get_book_author(soup_of_book)}\n\n{get_book_info(soup_of_book)}\n\n{get_book_top(soup_of_book)}\n{get_book_rating(soup_of_book)}'
-		link = get_book_link(soup_of_book)
-		cover = get_book_cover(soup_of_book)
-		return link, cover, description
-	except:
-		compile_advice_top()
+def compile_advice_genre(genre):
+    soup_of_genre_page = get_soup('https://www.goodreads.com/shelf/show/' + genre)
+    book_link = make_book_choice_from_page(soup_of_genre_page)
+    soup_of_book = get_soup('https://www.goodreads.com/' + book_link)
+    full_book_info = get_full_book_info(soup_of_book)
+    return full_book_info
 
 
 def compile_advice_random():
-	'''Случайный выбор'''
-	try:
-		soup_of_book = get_soup('https://www.livelib.ru/book/random')
-		description = f'{get_book_title(soup_of_book)}\n\n{get_book_author(soup_of_book)}\n\n{get_book_info(soup_of_book)}\n\n{get_book_rating(soup_of_book)}'
-		link = get_book_link(soup_of_book)
-		cover = get_book_cover(soup_of_book)
-		return link, cover, description
-	except:
-		compile_advice_random()
+    soup_of_random_book = get_soup('https://www.goodreads.com/book/random')
+    random_book_url = soup_of_random_book.find('link', {'rel':"canonical"}).get('href')
+    soup_of_book = get_soup('https://www.goodreads.com/en/book/show' + random_book_url[35:])
+    full_book_info = get_full_book_info(soup_of_book)
+    return full_book_info
 
 
-def get_page_soup(genre):
-	'''Жанры - Выбор номера страницы в жанре'''
-	page = random.randint(1, 15)
-	return f'https://www.livelib.ru/genre/{genre}/best/listview/biglist/~{page}'
+def compile_advice_choice_awards(year):
+    soup_of_year_page = get_soup('https://www.goodreads.com/choiceawards/best-books-' + year)
+    book_link = make_book_choice_from_page(soup_of_year_page)
+    soup_of_book = get_soup('https://www.goodreads.com/' + book_link)
+    full_book_info = get_full_book_info(soup_of_book)
+    return full_book_info
 
 
-def make_choice_from_page(bsoup):
-	'''Жанры и Топ-100 - Ссылки на все книги со страницы'''
-	list_of_books = []
-	for book in bsoup.find_all('a', {'class':"brow-book-name with-cycle"}):
-		book = book.get('href')
-		list_of_books.append(book)
-	return random.choice(list_of_books)
-
-
-def get_book_link(bsoup):
-	return bsoup.find('link', {'rel':"canonical"}).get('href')
-
-
-def get_book_cover(bsoup):
-	raw_cover = bsoup.find('img', {'id':"main-image-book"}).get('src')
-	return raw_cover.replace("/200/", "/o/").replace("jpg", "jpeg")
-
-
-def get_book_title(bsoup):
-	raw_title = bsoup.find('span', {'itemprop':"name"}).string
-	return f'*{raw_title}*'
-
-
-def get_book_author(bsoup):
-	raw_author = bsoup.find('a', {'id':"book-author"}).get('title')
-	return f'Автор:  {raw_author}'
-
-
-def get_book_info(bsoup):
-	raw_info = bsoup.find('table', {'class':"compact"}).get_text().split()
-	return f'ISBN:  {raw_info[1]}\nГод издания:  {raw_info[4]}'
-
-
-def get_book_rating(bsoup):
-	raw_rating = bsoup.find('span', {'style':"margin-right: 5px"}).string
-	return f'*Средняя оценка:  {raw_rating}*'
-
-
-def get_book_top(bsoup):
-	raw_top = bsoup.find('a', {'class':"label-orange"}).string
-	return f'Место в топ-100:  {raw_top[1:3]}'
+def compile_advice_new_releases():
+    soup_of_genre_page = get_soup('https://www.goodreads.com/book/popular_by_date/')
+    book_link = make_book_choice_from_page(soup_of_genre_page)
+    soup_of_book = get_soup('https://www.goodreads.com/' + book_link)
+    full_book_info = get_full_book_info(soup_of_book)
+    return full_book_info
